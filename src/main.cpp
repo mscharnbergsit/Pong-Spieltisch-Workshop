7415aac40b95148e78ab5714867ca11cdf1666de
mats.scharnberg <mats.scharnberg@singular-it.de>
2023-11-05 09:10:44 +0100

    add pong

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,7 @@
 #include <Arduino.h>
 #include <FastLED.h>
+#include <LiquidCrystal_I2C.h>
+#include <time.h>
 
 #define LED_PIN 28
 //Set Matrix layout, used for calculating led position using XY method
@@ -33,9 +35,96 @@
 
 #define NUM_LEDS (MATRIX_WIDTH * MATRIX_HEIGHT)
 
+struct Ball {
+  int x;
+  int y;
+  int dirX;
+  int dirY;
+  int speed = 1;
+} ball;
+
+struct Paddle {
+  int x;
+  double y;
+  int width = 1;
+  double speed = 1;
+} paddle1, paddle2;
+
+bool gameIsRunning = false;
+int score[2];
+
 CRGB leds[NUM_LEDS];
+LiquidCrystal_I2C lcd(0x27, 16, 2);
+
+CRGB ballColor = CRGB::Red;
+CRGB paddleColor = CRGB::Blue;
+
+unsigned long gameStepInterval = 100;
+unsigned long lastGameStepTime = 0;
+
+unsigned long inputStepInterval = 50;
+unsigned long lastInputStepTime = 0;
 
 uint16_t XY( uint8_t x, uint8_t y);
+void initInputs();
+void initPlayers();
+void initBall();
+void movePaddle(Paddle &paddle, int dirY);
+void drawPaddle(Paddle paddle);
+void removePaddle(Paddle paddle);
+void moveBall();
+void bounceBallWall();
+void bounceBallPaddle();
+void drawBall();
+void removeBall();
+bool isCollidingWithPaddle(int x, int y);
+void updatePlayer1();
+void updatePlayer2();
+void updateBall();
+void checkGameStart(unsigned long currentMillis);
+void checkGameEnd();
+void checkGoal();
+void startGame();
+void endGame();
+
+void setup() {
+  Serial.begin(115200);
+  FastLED.addLeds<WS2812B, LED_PIN, GRB>(leds, NUM_LEDS);
+  FastLED.setBrightness(100);
+
+  lcd.init();
+  lcd.backlight();
+
+  initInputs();
+
+  lcd.setCursor(0, 0);
+  lcd.print("Press green");
+  lcd.setCursor(0, 1);
+  lcd.print("to start game");
+  srand(time(NULL));
+}
+
+void loop() {
+  unsigned long currentMillis = millis();
+  if(!gameIsRunning) {
+    checkGameStart(currentMillis);
+  }
+  else {
+    if(currentMillis - lastInputStepTime >= inputStepInterval) {
+      updatePlayer1();
+      updatePlayer2();
+      FastLED.show();
+      lastInputStepTime = currentMillis;
+    }
+    if(currentMillis - lastGameStepTime >= gameStepInterval) {
+      updateBall();
+      FastLED.show();
+      checkGoal(); 
+      lastGameStepTime = currentMillis;
+    } 
+    checkGameEnd();
+  }
+}
 
 void initInputs() {
   //Player 1 PIN Setup
@@ -68,7 +157,66 @@ void initInputs() {
   pinMode(PLAYER_2_JOYSTICK_Y_AXIS, INPUT);
 }
 
-void printInputs() {
+void checkGameStart(unsigned long currentMillis) {
+  if (!digitalRead(PLAYER_1_GREEN_PIN)) {
+    startGame();
+    lastGameStepTime = currentMillis;
+  }
+}
+void checkGameEnd() {
+  if (!digitalRead(PLAYER_1_RED_PIN)) {
+    endGame();
+  }
+}
+void checkGoal() {
+  if(ball.x == 0 || ball.x == MATRIX_WIDTH - 1) {
+    Serial.println("Goal");
+    if(ball.x == 0) {
+      score[1]++;
+    }
+    else {
+      score[0]++;
+    }
+    //goal animation
+    removeBall();
+    char buffer[6];
+    snprintf_P(buffer, sizeof(buffer), PSTR("%.2i:%.2i"), score[0], score[1]);
+    lcd.setCursor(0, 1);
+    lcd.print(buffer);
+    delay(500);
+    initBall();
+  }
+}
+void startGame() {
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print("Score:");
+  lcd.setCursor(0, 1);
+  lcd.print("00:00");
+  score[0] = 0;
+  score[1] = 0;
+  initPlayers();
+  initBall();
+  FastLED.show();
+  gameIsRunning = true;
+  delay(500);
+  
+}
+void endGame() {
+  removePaddle(paddle1);
+  removePaddle(paddle2);
+  removeBall();
+  FastLED.clearData();
+  FastLED.show();
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print("Press green");
+  lcd.setCursor(0, 1);
+  lcd.print("to start game");
+  gameIsRunning = false;
+}
+void updatePlayer1() {
+  removePaddle(paddle1);
   //Check all player 1 inputs and output if any is available
   if (!digitalRead(PLAYER_1_UP_PIN)) {
     Serial.println("Player 1 up");
@@ -91,19 +239,17 @@ void printInputs() {
   if (!digitalRead(PLAYER_1_JOYSTICK_BUTTON)) {
     Serial.println("Player 1 Joystick button");
   }
-  int player1X = analogRead(PLAYER_1_JOYSTICK_X_AXIS);
-  int player1Y = analogRead(PLAYER_1_JOYSTICK_Y_AXIS);
-  if (player1X < 450 || player1X > 580) {
-    Serial.print("Player 1 X: ");
-    Serial.println(player1X);
+  int player1Y = analogRead(PLAYER_1_JOYSTICK_X_AXIS);
+  if (player1Y < 450) {
+    movePaddle(paddle1, 1);
   }
-   if (player1Y < 450 || player1Y > 580) {
-    Serial.print("Player 1 Y: ");
-    Serial.println(player1Y);
+  if (player1Y > 580) {
+    movePaddle(paddle1, -1);
   }
-
-
-
+  drawPaddle(paddle1);
+}
+void updatePlayer2() {
+  removePaddle(paddle2);
   //Check all player 2 inputs and output if any is available
   if (!digitalRead(PLAYER_2_UP_PIN)) {
     Serial.println("Player 2 up");
@@ -120,42 +266,95 @@ void printInputs() {
   if (!digitalRead(PLAYER_2_JOYSTICK_BUTTON)) {
     Serial.println("Player 2 Joystick button");
   }
-  int player2X = analogRead(PLAYER_2_JOYSTICK_X_AXIS);
-  int player2Y = analogRead(PLAYER_2_JOYSTICK_Y_AXIS);
-  if (player2X < 450 || player2X > 580) {
-    Serial.print("Player 2 X: ");
-    Serial.println(player2X);
+  int player2Y = analogRead(PLAYER_2_JOYSTICK_X_AXIS);
+  if (player2Y < 450) {
+    movePaddle(paddle2, 1);
   }
-   if (player2Y < 450 || player2Y > 580) {
-    Serial.print("Player 2 Y: ");
-    Serial.println(player2Y);
+  if (player2Y > 580) {
+    movePaddle(paddle2, -1);
+  }
+
+  drawPaddle(paddle2);
+}
+void updateBall() {
+  removeBall();
+  moveBall();
+  drawBall();
+}
+void initPlayers() {
+  paddle1.y = (MATRIX_HEIGHT - 1) / 2;
+  paddle1.x = 1;
+  paddle1.width = 3;
+  paddle2.y = (MATRIX_HEIGHT - 1) / 2;
+  paddle2.x = MATRIX_WIDTH - 2;
+  paddle2.width = 3;
+}
+void initBall() {
+  ball.dirX = rand() % 2 == 1 ? 1 : -1;
+  ball.dirY = rand() % 3 - 1;
+  ball.x = (MATRIX_WIDTH - 1) / 2 + (ball.dirX == 1 ? 0 : 1);
+  ball.y = rand() % (MATRIX_HEIGHT - 2) + 1;
+}
+void movePaddle(Paddle &paddle, int dirY) {
+  double newPosition = paddle.y + (dirY * paddle.speed);
+  bool isValidPosition =  newPosition >= 0 && newPosition + paddle.width <= MATRIX_HEIGHT;
+  paddle.y = isValidPosition ? newPosition : paddle.y;
+}
+void drawPaddle(Paddle paddle) {
+  CRGB color = paddleColor;
+  if(paddle.speed < 0)
+    color = blend(color, CRGB::Red, 125);
+  if(abs(paddle.speed) < 1)
+    color = blend(color, CRGB::Yellow, 125);
+  for (int i = 0; i < paddle.width; i++)
+  {
+    leds[XY(paddle.x, round(paddle.y) + i)] = color;
+  } 
+}
+void removePaddle(Paddle paddle) {
+  for (int i = 0; i < paddle.width; i++)
+  {
+    leds[XY(paddle.x, round(paddle.y) + i)] = CRGB::Black;
   }
 }
-
-void initLeds() {
-  for (int i = 0; i < MATRIX_WIDTH; i++) {
-    for (int j = 0; j < MATRIX_HEIGHT; j++) {
-      float x = (float) i / (float) MATRIX_WIDTH;
-      float y = (float) j / (float) MATRIX_HEIGHT;
-      leds[ XY(i, j) ] = CRGB(0, (int)(255.0 * x), 255 - (int)(255.0 * y));
-    }
-    FastLED.show();
-    delay(50);
+void moveBall() {
+  int nextX = ball.x + ball.dirX;
+  int nextY = ball.y + ball.dirY;
+  if(nextY < 0 || nextY > MATRIX_HEIGHT - 1) {
+    bounceBallWall();
+    nextY = ball.y + ball.dirY;
   }
+  if(isCollidingWithPaddle(nextX, nextY)) {
+    bounceBallPaddle();
+    nextX = ball.x + ball.dirX;
+    nextY = ball.y + ball.dirY;
+  }
+  if(nextY < 0 || nextY > MATRIX_HEIGHT - 1) {
+    bounceBallWall();
+    nextY = ball.y + ball.dirY;
+  }
+  ball.x = nextX;
+  ball.y = nextY;
 }
-
-void setup() {
-  // put your setup code here, to run once:
-  Serial.begin(115200);
-  FastLED.addLeds<WS2812B, LED_PIN, GRB>(leds, NUM_LEDS);
-  FastLED.setBrightness(255);
-  initInputs();
-  initLeds();
+bool isCollidingWithPaddle(int x, int y) {
+  return (x == paddle1.x && y >= round(paddle1.y) && y <= round(paddle1.y) + paddle1.width - 1 ) || (x == paddle2.x && y >= round(paddle2.y) && y <= round(paddle2.y) + paddle2.width - 1 );
 }
-
-void loop() {
-  // put your main code here, to run repeatedly:
-  printInputs();
+void bounceBallWall() {
+  Serial.println("Bounce Wall");
+  ball.dirY = ball.dirY * -1;
+}
+void bounceBallPaddle() {
+  Serial.println("Bounce Paddle");
+  ball.dirY = (rand() % 3) - 1;
+  ball.dirX = ball.dirX * -1;
+}
+void removeBall() {
+  uint16_t index = XY(ball.x, ball.y);
+  if(leds[index] == ballColor)
+    leds[index] = CRGB::Black;
+}
+void drawBall() {
+  leds[XY(ball.x, ball.y)] = ballColor;
 }
 
 /**
